bf75d1caaa7c633dd597eee8253b9664e0c171d1
Vinicius Soares <vsdc.snf19@uea.edu.br>
2020-11-16 15:54:45 -0400

    adicionado nivel 2 / nova armadilha: plataforma instável

---
+++ New Unity Project/Assets/Scripts/Falling_Platform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling_Platform : MonoBehaviour
+{
+
+    public float fallingTime;
+
+    private TargetJoint2D joint;
+    private BoxCollider2D collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = GetComponent<TargetJoint2D>();
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Invoke("falling",fallingTime);
+        }
+    }
+
+    void falling()
+    {
+        joint.enabled = false;
+        collider.isTrigger = true;
+    }
+}
