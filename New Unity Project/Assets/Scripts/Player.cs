c7f877de255e71b3074aa457d1f59fbc86cb954a
Vinicius Soares <vsdc.snf19@uea.edu.br>
2020-11-15 22:19:58 -0400

    adicionado animações do jogador

--- New Unity Project/Assets/Scripts/Player.cs
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -9,11 +9,14 @@ public class Player : MonoBehaviour
 
     public bool isJumping;
     public bool doubleJump;
+
     private Rigidbody2D rig;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,6 +30,20 @@ public class Player : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+        if(Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);  
+        }
+        if(Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+        if(Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("walk", false);
+        }
     }
 
     void Jump()
@@ -37,6 +54,9 @@ public class Player : MonoBehaviour
             {
                 rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
                 doubleJump = true;
+                anim.SetBool("walk", false);
+                anim.SetBool("jump", true);
+                
             }
             else
             {
@@ -55,6 +75,8 @@ public class Player : MonoBehaviour
         if (collision.gameObject.layer == 8)
         {
             isJumping = false;
+            anim.SetBool("jump", false);
+            anim.SetBool("walk", true);
         }
     }
 
@@ -63,6 +85,8 @@ public class Player : MonoBehaviour
         if (collision.gameObject.layer == 8)
         {
             isJumping = true;
+            anim.SetBool("walk", false);
+            anim.SetBool("jump", true);
         }
     }
 }
